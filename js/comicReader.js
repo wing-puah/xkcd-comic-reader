06e7ff0d2ca2451e7c9c9309a7b143b135e52ff0
wing-puah <puahhuiying@gmail.com>
2021-08-19 19:23:23 +0800

    finish logic

--- js/comicReader.js
+++ js/comicReader.js
@@ -3,35 +3,136 @@ const defaultSettings = {
   start: 1,
 };
 
+const createFetchQuery = function () {
+  const query = utils.getUrlQuery();
+  const start = Number(query.start);
+  const perPage = Number(query.perPage);
+
+  function getDefault() {
+    return {
+      start: start || defaultSettings.start,
+      perPage: perPage || defaultSettings.perPage,
+    };
+  }
+
+  function getPrevious() {
+    const newPage = start - perPage;
+    const finalPage = newPage > 0 ? newPage : 1;
+    return { start: finalPage, perPage: perPage };
+  }
+
+  function getNext() {
+    return { start: start + perPage, perPage };
+  }
+
+  function getRandom() {
+    const sign = Math.random() > 0.5 ? 'add' : 'minus';
+    const randomInt = utils.getRandomInt(3, 20);
+    if (sign === 'add') {
+      return { start: start + randomInt, perPage };
+    } else {
+      const newPage = start - randomInt;
+      const finalPage = newPage > 0 ? newPage : start + randomInt;
+      return { start: finalPage, perPage };
+    }
+  }
+
+  function getPage(number) {
+    const newNumber = Number(number) - Math.floor(perPage / 2);
+    const _finalNumber = newNumber < 1 ? 1 : newNumber;
+
+    return { start: _finalNumber, perPage };
+  }
+
+  function getSelection(numOfSelection) {
+    return { start: start, perPage: numOfSelection };
+  }
+
+  return {
+    query,
+    getDefault,
+    getPrevious,
+    getNext,
+    getRandom,
+    getSelection,
+    getPage,
+  };
+};
+
+function initControlQueryAndPopulate(initialQuery) {
+  const selectorDropdown = document.getElementById('count');
+  const { perPage } = initialQuery;
+  selectorDropdown.value = perPage;
+}
+
 (function comicReader() {
   // initial fetching
-  initFetchComic();
+  const initialQuery = createFetchQuery().getDefault();
+  initFetchComic(initialQuery);
+  initControlQueryAndPopulate(initialQuery);
 
-  const prevButton = document.getElementById('prev');
-  const nextButton = document.getElementById('next');
-  const randomButton = document.getElementById('random');
+  const prevButton = document.getElementsByClassName('prev');
+  const nextButton = document.getElementsByClassName('next');
+  const randomButton = document.getElementsByClassName('random');
+
   const selectorDropdown = document.getElementById('count');
+  const form = document.getElementById('page');
 
-  prevButton.addEventListener('click', function () {
-    initFetchComic('prev');
+  createEventHandler(prevButton, 'getPrevious');
+  createEventHandler(nextButton, 'getNext');
+  createEventHandler(randomButton, 'getRandom');
+
+  function createEventHandler(elementIterator, initType) {
+    for (let el of elementIterator) {
+      el.addEventListener('click', function () {
+        initFetchComic(createFetchQuery()[initType]());
+      });
+    }
+  }
+
+  selectorDropdown.addEventListener('change', function (e) {
+    const _value = Number(e.target.value);
+    const { query, getSelection } = createFetchQuery();
+
+    if (Number(query.perPage) > _value) {
+      const comicContainer = document.getElementById('comic');
+      removeImageNodeFromIndex(_value - 1, comicContainer);
+      const newQuery = getSelection(_value);
+      utils.updateURLParams(newQuery);
+    } else {
+      initFetchComic(getSelection(_value));
+    }
   });
 
-  nextButton.addEventListener('click', function () {
-    initFetchComic('next');
+  const input = form.querySelector('input[name="pageNum"]');
+  input.addEventListener('change', function () {
+    pageController.hideFormError();
   });
 
-  randomButton.addEventListener('click', function () {
-    initFetchComic('random');
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    try {
+      var formData = Object.fromEntries(new FormData(form));
+      const newPageNumber = parseInt(formData['pageNum']);
+
+      if (newPageNumber < 0) {
+        pageController.addFormError(`Negative number is not allowed`);
+        return;
+      }
+
+      initFetchComic(createFetchQuery().getPage(newPageNumber));
+    } catch (error) {
+      console.error(`Issue with fetching: ${error}`);
+      pageController.addFormError(error);
+    }
   });
 
-  selectorDropdown.addEventListener('change', function () {
-    console.log('drop down change');
-    //   initFetchComic('select')
-  });
+  async function initFetchComic(query) {
+    enableComicButtons();
 
-  async function initFetchComic(key) {
-    const query = createFetchParams(key);
     utils.updateURLParams(query);
+
+    clearAllComics();
     pageController.showLoadingIndicator();
 
     try {
@@ -39,8 +140,9 @@ const defaultSettings = {
 
       if (!comic) {
         handleEmptyComic();
+        return;
       }
-      // if no comic => displayZeroComicError
+
       populateComic(comic, query.perPage);
       pageController.hideLoadingIndicator();
     } catch (error) {
@@ -51,91 +153,19 @@ const defaultSettings = {
     }
   }
 
-  function createFetchParams(key) {
-    const query = utils.getUrlQuery();
-    const start = Number(query.start);
-    const perPage = Number(query.perPage);
+  function enableComicButtons() {
+    const footerButtons = document.querySelector('.buttons__footer');
+    footerButtons.classList.replace('d-none', 'd-flex');
 
-    switch (key) {
-      case 'next':
-        return { start: start + perPage, perPage: query.perPage };
-
-      case 'prev':
-        return { start: start - perPage, perPage: query.perPage };
-
-      case 'random':
-        const sign = Math.random() > 0.5 ? 'add' : 'minus';
-        const randomInt = getRandomInt(1, 20);
-        if (sign === 'add') {
-          return { start: start + randomInt, perPage: query.perPage };
-        } else {
-          const newPage = start - randomInt;
-          const finalPage = newPage > 0 ? newPage : start + randomInt;
-          return { start: finalPage, perPage: query.perPage };
-        }
-
-      default:
-        return {
-          start: defaultSettings.start,
-          perPage: defaultSettings.perPage,
-        };
-    }
-  }
-
-  function fetchAndPopulateComic({ start, perPage }) {
-    const url = `https://intro-to-js-playground.vercel.app/api/xkcd-comics`;
-    const allPromises = Array.from({ length: perPage }, (v, i) => {
-      return fetch(`${url}/${Number(start) + i}`);
+    const comicButtons = document.querySelectorAll('.comic__button');
+    comicButtons.forEach((el) => {
+      el.disabled = false;
     });
-    console.log({ allPromises });
-
-    return Promise.allSettled(allPromises);
   }
 
-  async function populateComic(comics, numberOfComics) {
-    console.log({ comics, numberOfComics });
+  function clearAllComics() {
     const comicContainer = document.getElementById('comic');
-
-    let i = 0;
-    for (i; i < numberOfComics; i++) {
-      const currentComic = comics[i];
-
-      if (currentComic?.status === 'fulfilled' && currentComic?.value) {
-        const data = await currentComic.value.json();
-        populateSingleComicEl(data, i, comicContainer);
-        continue;
-      }
-      // break out if no comic found
-    }
-  }
-
-  function populateSingleComicEl(data, index, container) {
-    console.log({ data, childrean: container.children });
-    let comicItem;
-
-    if (container?.children?.length > index) {
-      comicItem = container.children[index];
-    }
-
-    if (!comicItem) {
-      comicItem = createChildContainer(index, container);
-    }
-
-    const { safe_title, alt, img, transcript } = data;
-    const imgEl = comicItem.getElementsByTagName('img')[0];
-    imgEl.src = img;
-    imgEl.title = safe_title;
-  }
-
-  function createChildContainer(index, container) {
-    const comicItemContainer = document.createElement('div');
-    comicItemContainer.setAttribute('id', `comic-${index + 1}`);
-
-    const comicItemGraphic = document.createElement('img');
-    comicItemContainer.appendChild(comicItemGraphic);
-
-    container.appendChild(comicItemContainer);
-    return comicItemContainer;
+    comicContainer.innerHTML = '';
   }
 
   function handleEmptyComic() {
@@ -144,9 +174,120 @@ const defaultSettings = {
   }
 
   function disabledComicButtons() {
+    const footerButtons = document.querySelector('.buttons__footer');
+    footerButtons.classList.replace('d-flex', 'd-none');
+
     const comicButtons = document.querySelectorAll('.comic__button');
     comicButtons.forEach((el) => {
       el.disabled = true;
     });
+  }
+
+  function fetchAndPopulateComic({ start, perPage }) {
+    const url = `https://intro-to-js-playground.vercel.app/api/xkcd-comics`;
+    const allPromises = Array.from({ length: perPage }, (v, i) => {
+      return fetch(`${url}/${Number(start) + i}`);
+    });
+
+    return Promise.allSettled(allPromises);
+  }
+
+  async function populateComic(comics, numberOfComics) {
+    const comicContainer = document.getElementById('comic');
+
+    let i = 0;
+    for (i; i < numberOfComics; i++) {
+      const currentComic = comics[i];
+
+      if (
+        currentComic?.status === 'fulfilled' &&
+        currentComic?.value?.status === 200
+      ) {
+        const data = await currentComic.value.json();
+        populateSingleComicEl(data, i, comicContainer);
+        continue;
+      }
+
+      if (i === 0) {
+        handleEmptyComic();
+        break;
+      }
+
+      populateEnd(i, comicContainer);
+      removeImageNodeFromIndex(i + 1, comicContainer);
+      break;
+    }
+  }
+
+  function populateSingleComicEl(data, index, container) {
+    let comicItem;
+
+    if (container?.children?.length > index) {
+      comicItem = container.children[index];
+      // the End tag will not have an image source
+      if (container.children[index].getElementsByTagName('img').length === 0) {
+        createImgAndTextAndAppend(comicItem);
+      }
+    }
+
+    if (!comicItem) {
+      comicItem = createChildContainer(index, container);
+    }
+
+    const { safe_title, alt, img, num } = data;
+    const titleEl = comicItem.querySelector('.comic__title');
+    titleEl.innerText = `${num}. ${safe_title}`;
+
+    const imgEl = comicItem.getElementsByTagName('img')[0];
+    imgEl.src = img;
+    imgEl.title = alt;
+    imgEl.setAttribute('data-num', num);
+  }
+
+  function createChildContainer(index, container) {
+    const comicItemContainer = document.createElement('div');
+    createImgAndTextAndAppend(comicItemContainer, index);
+    container.appendChild(comicItemContainer);
+    return comicItemContainer;
+  }
+
+  function createImgAndTextAndAppend(container, index) {
+    container.innerText = '';
+    container.classList.add('comic__container');
+    container.setAttribute('id', `comic-${index + 1}`);
+    const comicItemTitle = document.createElement('div');
+    comicItemTitle.classList.add('comic__title', 'text-center', 'text-bold');
+    const comicItemGraphic = document.createElement('img');
+    container.append(comicItemTitle);
+    container.appendChild(comicItemGraphic);
+    return container;
+  }
+
+  function populateEnd(index, container) {
+    let comicItem;
+
+    if (container?.children?.length > index) {
+      comicItem = container.children[index];
+      comicItem.removeAttribute('id');
+      comicItem.removeAttribute('data-num');
+    }
+
+    if (!comicItem) {
+      comicItem = document.createElement('div');
+      container.appendChild(comicItem);
+    }
+
+    comicItem.innerText = 'End';
+    nextButton.disabled = true;
+  }
+
+  function removeImageNodeFromIndex(index, container) {
+    if (container?.children?.length > index) {
+      let i = index;
+      for (i; i < container.children.length; i++) {
+        const child = container.children[i];
+        container.removeChild(child);
+      }
+    }
   }
 })();
--- js/pageController.js
+++ js/pageController.js
@@ -1,10 +1,50 @@
 const pageController = (function () {
+  const loadingIndicator = document.querySelector('.lds-spinner___container');
   function showLoadingIndicator() {
-    console.log('show');
+    loadingIndicator.classList.replace('d-none', 'd-flex');
   }
-  function hideLoadingIndicator() {}
-  function showError(errorMessage) {}
-  function hideError() {}
 
-  return { showLoadingIndicator, hideLoadingIndicator, showError, hideError };
+  function hideLoadingIndicator() {
+    loadingIndicator.classList.replace('d-flex', 'd-none');
+  }
+
+  let timerId;
+  function showError(errorMessage) {
+    console.log('showing');
+    clearTimeout(timerId);
+    const snackbar = document.querySelector('.snackbar');
+    snackbar.innerText = errorMessage;
+    snackbar.classList.replace('d-none', 'd-block');
+
+    timerId = setTimeout(function () {
+      hideError();
+    }, 3000);
+  }
+  function hideError() {
+    console.log('hiding');
+    const snackbar = document.querySelector('.snackbar');
+    snackbar.classList.replace('d-block', 'd-none');
+  }
+
+  function addFormError(error) {
+    const errorEl = document.querySelector('form .error');
+    const errorText = typeof error === 'string' ? error : JSON.stringify(error);
+    errorEl.innerText = errorText;
+    errorEl.classList.replace('display-none', 'display-inline');
+  }
+
+  function hideFormError() {
+    const errorEl = document.querySelector('form .error');
+    errorEl.innerText = '';
+    errorEl.classList.replace('display-inline', 'display-none');
+  }
+
+  return {
+    showLoadingIndicator,
+    hideLoadingIndicator,
+    showError,
+    hideError,
+    addFormError,
+    hideFormError,
+  };
 })();
